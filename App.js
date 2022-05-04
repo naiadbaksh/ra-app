b95b01d70780d8e9ba7ae610f31d564dd268e9f2
Naiad Baksh <naiadbaksh@gmail.com>
2022-05-04 16:28:28 -0400

    setting up axios and making API call successfully

--- App.js
+++ App.js
@@ -1,12 +1,31 @@
 import { StatusBar } from "expo-status-bar";
+import React, { useEffect } from "react";
+import Test from "./test.component";
 import { StyleSheet, Text, View } from "react-native";
+import axios from "axios";
 
 export default function App() {
+  const getData = async () => {
+    const { data } = await axios.get("http://localhost:1337/api/products", {
+      headers: {
+        Authorization:
+          "Bearer dda20632975e9a51dc0c012c65bf902c86d1c600f345d0205bb7e51006ca6e174addb5db922c67aec5ca969e85c44eb725fece71a67a92e2ac669407c55ceb94650fe859e434b045a24d681471dcaf5c3bffe1dcc016d29104cd7128da439782d68c8f99adacabdcf75e0c236f6026504ae9707e978e8a77ad043bd60ab6b1a2",
+      },
+    });
+
+    console.log(data);
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
   return (
     <View style={styles.container}>
       <Text style={{ color: "#888", fontSize: 18 }}>
-        This is the whole folder in VS Code.
+        Testing again in VSCode.
       </Text>
+      <Test></Test>
       <StatusBar style="auto" />
     </View>
   );
---
+++ test.component.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Test = () => {
+  return (
+    <div>
+      <h2>This is a test component in Expo.</h2>
+    </div>
+  );
+};
+
+export default Test;
+
+// Token
+// dda20632975e9a51dc0c012c65bf902c86d1c600f345d0205bb7e51006ca6e174addb5db922c67aec5ca969e85c44eb725fece71a67a92e2ac669407c55ceb94650fe859e434b045a24d681471dcaf5c3bffe1dcc016d29104cd7128da439782d68c8f99adacabdcf75e0c236f6026504ae9707e978e8a77ad043bd60ab6b1a2;
